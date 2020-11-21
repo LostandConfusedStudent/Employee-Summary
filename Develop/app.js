ae556b9e6bb164a4fc04e59f256620ec7a432f73
pausun <pfsun9999@msn.com>
2020-11-20 19:31:12 -0800

    Wrote manager class and worked on app.js to gather information

--- Develop/app.js
+++ Develop/app.js
@@ -4,6 +4,7 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const prompt = inquirer.createPromptModule();
 const employees = [];
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
@@ -14,7 +15,7 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-const inquirer = require("inquirer");
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
@@ -44,8 +45,56 @@ const questions = [{
     name: "email"
 }];
 
+const manager = {
+    type: "number",
+    message: "What's your office number?",
+    name: "office"
+}
+
+const workerType = [{
+    type: "list",
+    message: "Add a team member",
+    name: "role",
+    choices: [
+        "Engineer",
+        "Intern",
+        "No more"
+    ]
+}];
+
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+// Function to initiate the program
+function init() {
+    prompt([
+        ...questions,
+        manager
+    ]).then(({name, id, email, office}) => {
+        let manager = new Manager(name, id, email, office);
+        // Blank answers not accepted
+        if (name !== "" && id !== "" && office !== "") {
+            // Push to employees array
+            employees.push(manager);
+            makeWorkers();
+        };
+    });
+};
+
+function makeWorkers() {
+    prompt(workerType).then((data) => {
+        if (data.role === "engineer") {
+            engineer();
+        } else if (data.role === "intern") {
+            intern();
+        } else {
+            writeToFile("./output/team.html", render(employees));
+        };
+    });
+};
+
+// Function call to initiate program
+init();
--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,20 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+// extend employee
+const Employee = require("./Employee");
+
+class Manager extends Employee {
+    constructor (name, id, email, office) {
+        super (name, id, email);
+        this.office = office;
+    }
+    getRole() {
+        const role = "Manager";
+        return role;
+    }
+    getOffice() {
+        const office = this.office;
+        return office;
+    }
+}
+
+module.exports = Manager;
